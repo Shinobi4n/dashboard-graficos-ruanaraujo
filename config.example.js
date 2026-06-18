432571764a1d9dccf8a5292a054cb808f660a19d
Shinobi4n <ninjeneltre11@gmail.com>
2026-06-18 09:40:25 -0300

    Config: adiciona arquivo de chaves config.js para a vercel

--- config.example.js
+++ config.example.js
@@ -1,6 +1,3 @@
-// config.example.js
-// Copie este arquivo para `config.js` e preencha com suas chaves reais.
-// NÃO comite `config.js` com chaves reais no repositório público.
 
 window.APP_CONFIG = {
   APP_ID: 'YOUR_APP_ID_HERE',
